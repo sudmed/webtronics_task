58e1446fc50bc31eac60958459ae88eb33b23d03
Dmitriy Pashkov <pashkov.dmitriy@gmail.com>
2023-01-05 22:04:56 +0300

    Update main.tf

--- 2/main.tf
+++ 2/main.tf
@@ -1,30 +1,30 @@
 # Create VM in yandex.cloud
 resource "yandex_compute_instance" "VPS" {
-  platform_id = var.master_platform_id
-  count       = var.master_count
+  platform_id = var.vps_platform_id
+  count       = var.vps_count
   name        = "VPS"
 
   resources {
-    cores         = var.master_cores
-    memory        = var.master_ram
-    core_fraction = var.master_core_fraction
+    cores         = var.vps_cores
+    memory        = var.vps_ram
+    core_fraction = var.vps_core_fraction
   }
 
   boot_disk {
     initialize_params {
       image_id = var.image_id
-      size     = var.master_boot_disk_size
+      size     = var.vps_boot_disk_size
     }
   }
 
   network_interface {
     subnet_id = var.subnet_id
-    nat       = var.master_nat
+    nat       = var.vps_nat
   }
 
   metadata = {
     ssh-keys           = "ubuntu:${file("keys/id_rsa.pub")}"
-    serial-port-enable = var.master_serial-port-enable
+    serial-port-enable = var.vps_serial-port-enable
   }
 
   connection {
