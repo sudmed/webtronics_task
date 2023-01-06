6109ab4222409d1cc0643ff7633c8665e4d09686
Dmitriy Pashkov <pashkov.dmitriy@gmail.com>
2023-01-06 17:05:16 +0300

    Create script.sh

---
+++ 4/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# remove empty lines in file
+sed -i '/^$/d' file.txt
+
+# parse file line by line
+cat file.txt | while read line
+do
+   # if there is a trailing slash, it is a directory. Make dir only
+   [[ "${line}" == */ ]] && mkdir -p "${line}"
+
+   # if there isn't a trailing slash, it is a file. Touch file and make dir (if necessary)
+   [[ "${line}" != */ ]] && mkdir -p "$(dirname "$line")/" && touch "${line}"
+done
